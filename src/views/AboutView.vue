d1431008f946a9f91688b19a6158bc43c3c059e5
Samsethy <ratanak.khoeurn@student.passerellesnumeriques.org>
2024-08-10 13:48:28 +0700

    about page ratanak

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,22 +1,342 @@
 <template>
   <div class="about">
-    <NavBar/>
-    <h1>This is an about page!</h1>
+    <NavBar />
+    <div class="container">
+      <section class="banner"></section>
+      <div class="history">
+        <h2>Bank History</h2>
+        <p class="bank_history">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut .
+        </p>
+      </div>
+      <div class="grid-container bank_image">
+        <div class="grid-container-left">
+          <div class="grid-item image_1">
+            <img src="@/assets/image/image_1.png" alt="History 1" />
+          </div>
+          <div class="grid-item image_2">
+            <img src="@/assets/image/image_2.png" alt="History 2" />
+          </div>
+        </div>
+        <div class="grid-container-right">
+          <div class="grid-item image_3">
+            <img src="@/assets/image/image_3.png" alt="History 3" />
+          </div>
+          <div class="grid-item-right_bottom">
+            <div class="grid-item black-box">
+              <h6>What we do</h6>
+              <h7>Empowering Financial Journey</h7>
+              <a class="read-article"
+                >Read Article <img src="@/assets/image/cursor.png" alt=""
+              /></a>
+            </div>
+            <div class="grid-item image_4">
+              <img src="@/assets/image/image_4.png" alt="History 4" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <hr />
+      <div class="bank_info">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias
+          voluptas, reprehenderit debitis adipisci quidem et nemo non? Quaerat
+          vitae ratione quod explicabo totam atque omnis voluptates dolor
+          impedit, minima necessitatibus tempora quos est, error dicta sapiente
+          velit facilis laborum ipsum libero repudiandae, cumque nemo ex!
+          Similique porro ipsa suscipit beatae earum itaque amet eius, numquam,
+          debitis soluta non deserunt quos! Maxime, voluptates rem ad iusto
+          perferendis consequuntur tempore. Perspiciatis quos corporis debitis
+          quam blanditiis ipsa pariatur nulla illo quibusdam quod quia est
+          doloremque at dolor eveniet distinctio, laboriosam dolorum sit
+          dolorem. Laboriosam, cupiditate deserunt itaque hic optio, nisi
+          consectetur ipsam modi earum ut,<br /><br />
+          debitis distinctio voluptates odit quo praesentium molestiae id maxime
+          fugit natus suscipit autem non dolorum accusamus? Commodi voluptates
+          modi dolore asperiores non, aperiam tempora a, adipisci aut, itaque
+          culpa labore iste dolor placeat atque quasi laborum maiores quam
+          deleniti corporis vitae aspernatur odit sapiente? Saepe cum sit
+          dignissimos eum. Quas libero assumenda laudantium corrupti tempora
+          vitae delectus eum odio cupiditate. Assumenda sint voluptates eaque
+          veritatis repellat sit asperiores soluta quae. Provident sunt
+          excepturi illum sint placeat nisi? Laudantium incidunt neque nostrum
+          fugit quis suscipit quisquam magni vel, accusamus at illo aspernatur
+          officiis sit, dicta reiciendis dolore minima harum. <br /><br />
+          Soluta inventore, nisi quisquam, debitis molestias aspernatur
+          repellendus numquam vero dolorem quidem deleniti optio excepturi
+          culpa? Hic optio distinctio architecto eum omnis quibusdam, ratione
+          quaerat fugit culpa? Perspiciatis unde aliquam fugit, mollitia cum vel
+          dignissimos eligendi iste autem similique eius ullam ipsa tempora
+          cumque rem corporis molestias ratione laboriosam atque repudiandae
+          veritatis nostrum aspernatur alias. Ratione eius minus, assumenda iure
+          officia fuga. Perferendis voluptas laudantium enim aperiam, ad itaque
+          et quod, consequuntur sint alias, assumenda eos illo beatae asperiores
+          distinctio? Beatae totam perspiciatis voluptate fuga quibusdam?
+          Excepturi nulla fuga asperiores quia officiis itaque quos veritatis
+          rerum quidem. Quasi, optio.
+        </p>
+      </div>
+      <div class="mis_vis">
+        <div class="vission">
+          <div class="grid-item-vission">
+            <img src="@/assets/image/vission.png" alt="Vission" />
+            <div class="vis_info">
+              <h1>Vission</h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Officiis ullam quas aspernatur voluptates qui aperiam tempora
+                nam debitis tenetur, consectetur nisi, labore eius excepturi
+                pariatur ipsa repellendus. In, harum eos. <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+                adipisci ducimus vel laboriosam delectus alias eos magnam cum
+                sapiente odio nam facilis hic repudiandae ab minima, corporis,
+                quis laudantium quia?
+              </p>
+            </div>
+          </div>
+          <div class="grid-item-mission">
+            <div class="mission_info">
+              <h1>Mission</h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Officiis ullam quas aspernatur voluptates qui aperiam tempora
+                nam debitis tenetur, consectetur nisi, labore eius excepturi
+                pariatur ipsa repellendus. In, harum eos. <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+                adipisci ducimus vel laboriosam delectus alias eos magnam cum
+                sapiente odio nam facilis hic repudiandae ab minima, corporis,
+                quis laudantium quia?
+              </p>
+            </div>
+            <img src="@/assets/image/mission.png" alt="mission" />
+          </div>
+        </div>
+      </div>
+      <div class="board_director">
+        <div class="grid-item board_left">
+          <h4 class="board_left_title">Meet our Board of Directors</h4>
+          <a class="view_members"
+            ><img src="@/assets/image/seeMembers.png" alt="view_member" /> See
+            all members</a
+          >
+          <div class="slide">
+            <img src="@/assets/image/row_left.png" alt="row_left" />
+            <img src="@/assets/image/row_right.png" alt="row_left" />
+          </div>
+        </div>
+        <div class="grid-item board_right">
+          <div class="card">
+            <div class="card-image">
+              <img class="activator" src="@/assets/image/mavy.png" alt="mavy" />
+            </div>
+            <div class="card-content">
+              <h4 class="card-title">Mavy Prestly</h4>
+              <p>Chief Executive Officer</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import NavBar from '@/components/NavBar.vue';
+import NavBar from "@/components/NavBar.vue";
 export default {
-  name: 'AboutView',
+  name: "AboutView",
   components: {
-    NavBar
-  }
-}
+    NavBar,
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.banner {
+  background-image: url("@/assets/image/about.png");
+  background-color: black;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  border-radius: 50px;
+  background-position: center;
+  padding: 10rem 0;
+}
+.history {
+  padding: 5rem 0;
+  text-align: center;
+}
+.bank_history {
+  width: 60%;
+  margin: 0 auto;
+  padding: 1rem;
+}
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
+}
+.grid-container-left {
+  display: grid;
+  grid-template-columns: repeat(1fr);
+  grid-template-rows: 2fr;
+  gap: 1rem;
+}
+.grid-container-right {
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  grid-template-rows: 2fr;
+  gap: 1rem;
+}
+.grid-item {
+  position: relative;
+}
+.grid-item img {
+  width: 100%;
+  /* border-radius: 10px; */
+}
+.black-box {
+  background-color: #000000;
+  color: white;
+  padding: 2rem;
+  display: flex;
+  margin-bottom: 50%;
+  width: 250px;
+  height: 230px;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 10px;
+}
+.grid-item-vission {
+  display: grid;
+  margin-top: 50px;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+}
+.grid-item-mission {
+  display: grid;
+  margin-top: 50px;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+}
+.grid-item-mission img,
+.grid-item-vission img {
+  width: 90%;
+  height: 350px;
+}
+.board_director {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  width: 100%;
+  margin-top: 60px;
+  /* background-color: grey; */
+  gap: 1rem;
+}
+.board_left {
+  display: grid;
+  background-color: #000000;
+  width: 80%;
+  border-radius: 0 50px 50px 0;
+  height: 350px;
+  grid-template-columns: repeat(1, 1fr);
+  gap: 1rem;
+}
+.board_left_title {
+  margin-left: 30px;
+  width: 190px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: white;
+}
+.view_members {
+  margin-left: 30px;
+  color: white;
+  /* gap: 20px; */
+}
+.board_right {
+  display: grid;
+  margin-left: -50%;
+  margin-top: 25px;
+  height: 300px;
+  border-left: 1px solid white;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
+}
+.card{
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  margin-left: -30px;
+  height: 300px;
+  width: 220px;
+}
+.card-image{
+  margin-left:-70%;
+  margin-top: -70%;
+  border-radius: 40px;
+  width: 220px;
+  height: 180px;
+}
+.card-image .activator{
+  border-radius: 10px;
+  width: 86%;
+  height: 100%;
 
+}
+.card-content{
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+  margin-left: -60%;  
+  margin-top: 20px;
+}
+.card-title{
+  font-size: 18px;
+  text-align: center;
+  align-items: center;
+}
+.view_members img,
+.slide img {
+  width: 30px;
+}
+.slide img {
+  margin-left: 30px;
+  margin-bottom: 30px;
+}
+.read-article {
+  color: white;
+  padding: 0.2rem;
+  cursor: pointer;
+  margin-top: 1rem;
+}
+.read-article img {
+  width: 20px;
+  margin-left: 10px;
+}
+.grid-item-right_bottom {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+}
+.image_1 > img {
+  width: 450px;
+  height: 450px;
+  margin-left: 20%;
+}
+.image_2 > img {
+  width: 560px;
+  height: 300px;
+}
+.image_3 > img {
+  margin-top: 100px;
+  width: 550px;
+  height: 300px;
+}
+.image_4 > img {
+  width: 450px;
+  height: 300px;
+}
 </style>
-
